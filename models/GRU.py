ac5e0618dc3fb3a4914e1ef28a8c58dab15a3204
djur1c <aleksdjurr1@gmail.com>
2025-05-21 01:29:45 +1000

    Update GRU.py

--- models/GRU.py
+++ models/GRU.py
@@ -1,1 +1,13 @@
-print("hello")
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Dropout, GRU
+from tensorflow.keras.models import Sequential
+
+def get_GRU(units):
+    
+    model = Sequential()
+    model.add(GRU(units[1], input_shape=(units[0], 1), return_sequences=True))
+    model.add(GRU(units[2]))
+    model.add(Dropout(0.2))
+    model.add(Dense(units[3], activation='sigmoid'))
+
+    return model
